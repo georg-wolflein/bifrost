1b2b7f17d68f97a7050e72c93bd16e58622c2660
akshaydeo <akshay@akshaydeo.com>
2026-06-08 13:02:13 +0530

    adds custom user agent mapping

--- ui/lib/store/apis/baseApi.ts
+++ ui/lib/store/apis/baseApi.ts
@@ -192,6 +192,7 @@ export const baseApi = createApi({
     "MCPSessions",
     "MCPPerUserHeaderCredentials",
     "FeatureFlags",
+    "UserAgentMappings",
     "Devices",
     "EdgeApps",
     "EdgeMCPServers",
